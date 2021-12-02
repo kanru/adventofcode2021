f62177dcd4ff5ec66581cff661ffb016c6d0b609
Kan-Ru Chen <kanru@kanru.info>
2021-12-03 00:00:43 +0900

    feat: impl Day2

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,23 @@
 mod sonar;
+mod submarine;
 
 use sonar::Sonar;
 use std::io;
+use submarine::Submarine;
 
 fn main() -> io::Result<()> {
     let input = "data/day01.txt";
     let reading = Sonar::sweep(input)?;
     println!("Day 1, part 1 => {}", Sonar::measure_width(&reading, 1));
     println!("Day 1, part 2 => {}", Sonar::measure_width(&reading, 3));
+
+    let input = "data/day02.txt";
+    let mut submarine_v1 = Submarine::v1();
+    let mut submarine_v2 = Submarine::v2();
+    let inst = Submarine::read_instruction(&input)?;
+    submarine_v1.run(&inst);
+    submarine_v2.run(&inst);
+    println!("Day 2, part 1 => {}", submarine_v1.report());
+    println!("Day 2, part 2 => {}", submarine_v2.report());
     Ok(())
 }
---
+++ src/submarine.rs
@@ -0,0 +1,152 @@
+use std::{
+    fs::File,
+    io::{self, prelude::*, BufReader},
+};
+
+pub struct Submarine {
+    pos: Position,
+    control: Box<dyn Control>,
+}
+
+struct Position {
+    x: i32,
+    depth: i32,
+    aim: i32,
+}
+
+trait Control {
+    fn forward(&self, prev: &Position, unit: i32) -> Position;
+    fn up(&self, prev: &Position, unit: i32) -> Position;
+    fn down(&self, prev: &Position, unit: i32) -> Position;
+}
+
+struct ControlV1;
+struct ControlV2;
+
+impl Control for ControlV1 {
+    fn forward(&self, prev: &Position, unit: i32) -> Position {
+        Position {
+            x: prev.x + unit,
+            ..*prev
+        }
+    }
+
+    fn up(&self, prev: &Position, unit: i32) -> Position {
+        Position {
+            depth: prev.depth - unit,
+            ..*prev
+        }
+    }
+
+    fn down(&self, prev: &Position, unit: i32) -> Position {
+        Position {
+            depth: prev.depth + unit,
+            ..*prev
+        }
+    }
+}
+
+impl Control for ControlV2 {
+    fn forward(&self, prev: &Position, unit: i32) -> Position {
+        Position {
+            x: prev.x + unit,
+            depth: prev.depth + prev.aim * unit,
+            ..*prev
+        }
+    }
+
+    fn up(&self, prev: &Position, unit: i32) -> Position {
+        Position {
+            aim: prev.aim - unit,
+            ..*prev
+        }
+    }
+
+    fn down(&self, prev: &Position, unit: i32) -> Position {
+        Position {
+            aim: prev.aim + unit,
+            ..*prev
+        }
+    }
+}
+
+impl Submarine {
+    pub fn v1() -> Submarine {
+        Submarine {
+            pos: Position {
+                x: 0,
+                depth: 0,
+                aim: 0,
+            },
+            control: Box::new(ControlV1),
+        }
+    }
+
+    pub fn v2() -> Submarine {
+        Submarine {
+            pos: Position {
+                x: 0,
+                depth: 0,
+                aim: 0,
+            },
+            control: Box::new(ControlV2),
+        }
+    }
+
+    pub fn read_instruction(input: &str) -> io::Result<Vec<String>> {
+        let file = File::open(input)?;
+        let reader = BufReader::new(file);
+        reader.lines().collect()
+    }
+
+    pub fn run(&mut self, input: &Vec<String>) {
+        for inst in input {
+            let mut split = inst.split(" ");
+            let dir = split.next().unwrap();
+            let unit = split.next().unwrap().parse::<i32>().unwrap();
+            match dir {
+                "up" => self.pos = self.control.up(&self.pos, unit),
+                "down" => self.pos = self.control.down(&self.pos, unit),
+                "forward" => self.pos = self.control.forward(&self.pos, unit),
+                _ => panic!("unknown instruction {}", dir),
+            }
+        }
+    }
+
+    pub fn report(&self) -> i32 {
+        self.pos.x * self.pos.depth
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Submarine;
+
+    #[test]
+    fn test_day2_part1() {
+        let mut submarine = Submarine::v1();
+        submarine.run(&vec![
+            "forward 5".to_string(),
+            "down 5".to_string(),
+            "forward 8".to_string(),
+            "up 3".to_string(),
+            "down 8".to_string(),
+            "forward 2".to_string(),
+        ]);
+        assert_eq!(150, submarine.report());
+    }
+
+    #[test]
+    fn test_day2_part2() {
+        let mut submarine = Submarine::v2();
+        submarine.run(&vec![
+            "forward 5".to_string(),
+            "down 5".to_string(),
+            "forward 8".to_string(),
+            "up 3".to_string(),
+            "down 8".to_string(),
+            "forward 2".to_string(),
+        ]);
+        assert_eq!(900, submarine.report());
+    }
+}
